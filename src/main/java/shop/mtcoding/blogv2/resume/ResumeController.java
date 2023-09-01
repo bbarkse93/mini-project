e6b94859b28874d0edb0294759a80579986ec313
park097 <alslalslclq24@naver.com>
2023-09-01 16:09:29 +0900

    resume delete 완성

--- src/main/java/shop/mtcoding/blogv2/resume/ResumeController.java
+++ src/main/java/shop/mtcoding/blogv2/resume/ResumeController.java
@@ -1,8 +1,23 @@
 package shop.mtcoding.blogv2.resume;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ResumeController {
+
+    @Autowired
+    ResumeService resumeService;
+
+     @PostMapping("/resume/{id}/delete")
+    public String delete(@PathVariable Integer id) {
+      
+        resumeService.deleteById(1); // 이렇게 하면 요청된 id에 해당하는 이력서를 삭제합니다.
+    
+        return "redirect:/test"; // userResumeList 페이지로 리디렉션
+    }
+         
 
 }
--- src/main/java/shop/mtcoding/blogv2/resume/ResumeService.java
+++ src/main/java/shop/mtcoding/blogv2/resume/ResumeService.java
@@ -1,8 +1,19 @@
 package shop.mtcoding.blogv2.resume;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ResumeService {
+
+    @Autowired
+    private ResumeRepository resumeRepository;
+
+    @Transactional
+    public void deleteById(Integer id) {
+        resumeRepository.deleteById(1);
+    }
+
 
 }
