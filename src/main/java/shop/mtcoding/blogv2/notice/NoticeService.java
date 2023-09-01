2ef4f14f81b8659ac13d338bfd11b9ecba845abf
saki8661 <135561587+saki8661@users.noreply.github.com>
2023-09-01 16:29:27 +0900

    채용공고 등록 (#3)

--- src/main/java/shop/mtcoding/blogv2/notice/NoticeService.java
+++ src/main/java/shop/mtcoding/blogv2/notice/NoticeService.java
@@ -1,8 +1,41 @@
 package shop.mtcoding.blogv2.notice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class NoticeService {
+
+    @Autowired
+    public NoticeRepository noticeRepository;
+
+    // 채용공고등록
+    @Transactional
+    public void 채용등록(NoticeRequest.SaveDTO saveDTO) {
+        Notice notice = Notice.builder()
+                .title(saveDTO.getTitle())
+                .companyName(saveDTO.getCompanyName())
+                .companyEmail(saveDTO.getCompanyEmail())
+                .phoneNumber(saveDTO.getPhoneNumber())
+                .companyPicUrl(saveDTO.getCompanyPicUrl())
+                .companyInfo(saveDTO.getCompanyInfo())
+                .location(saveDTO.getLocation())
+                .wishDutys(saveDTO.getWishDutys())
+                .wishSkills(saveDTO.getWishSkills())
+                .intake(saveDTO.getIntake())
+                .pay(saveDTO.getPay())
+                .period(saveDTO.getPeriod())
+                .qualification(saveDTO.getQualification())
+                .createdAt(saveDTO.getCreatedAt())
+                .build();
+        noticeRepository.save(notice);
+    }
+
+    // 채용공고수정
+
+    // 채용공고삭제
+
+    // 채용공고상세보기
 
 }
