fd1cb5b2f2dc8e75dd44e6f19ae0b79a043b009c
bbarkse93 <135561618+bbarkse93@users.noreply.github.com>
2023-09-01 17:18:40 +0900

    user 엔티티 변경 및 회원가입 수정 (#5)
    Co-authored-by: bbarkse93 <bbarkse93@gmail.com>

--- src/main/java/shop/mtcoding/blogv2/user/UserRequest.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserRequest.java
@@ -1,39 +1,31 @@
 package shop.mtcoding.blogv2.user;
 
-
-
 import java.sql.Timestamp;
-
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Getter;
 import lombok.Setter;
 
 public class UserRequest {
 
-    @Getter@Setter
+    @Getter
+    @Setter
     public static class JoinDTO {
         private String username;
         private String password;
-        private String personalName;
-        private String companyName;
-        private String personalBirth;
-        private String phoneNumber;
-        private String proprietaryNumber;
+        private String name;
+        private String telNumber;
+        private String registNumber;
         private String email;
-        private Integer personalid;
-        private Integer companyid;
+        private String picUrl;
+        private boolean distingush;
         private Timestamp createdAt;
     }
-    
-    @Getter@Setter
+
+    @Getter
+    @Setter
     public static class LoginDTO {
         private String usernmae;
         private String password;
     }
-
-
-
-      
 
 }
