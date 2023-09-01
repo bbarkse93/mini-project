ce6dfe4a1449373a4502cb1fb8f1d7c1ba26409d
bbarkse93 <bbarkse93@gmail.com>
2023-09-01 11:50:23 +0900

    고객센터 문의글 등록

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,8 +1,30 @@
 package shop.mtcoding.blogv2.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/customerService")
+    public String customerService() {
+        return "/board/customerService";
+    }
+
+    @GetMapping("/customerServiceWrite")
+    public String customerServiceWrite() {
+        return "/board/customerServiceWrite";
+    }
+
+    @PostMapping("/board/save")
+    public String customerServiceSave(BoardRequest.SaveDTO saveDTO) {
+        boardService.문의작성(saveDTO, 1);
+        return "redirect:/customerService";
+    }
 }
