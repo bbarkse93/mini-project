088ada9f6de1cac44e0c8553351f8933a552d4da
park097 <135561440+park097@users.noreply.github.com>
2023-09-03 03:20:54 +0900

    북마크 더미데이터 추가,화면 인티저 고쳐야 됨

--- src/main/java/shop/mtcoding/blogv2/bookmark/BookmarkController.java
+++ src/main/java/shop/mtcoding/blogv2/bookmark/BookmarkController.java
@@ -2,6 +2,8 @@ package shop.mtcoding.blogv2.bookmark;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +17,16 @@ public class BookmarkController {
     @Autowired
     private BookmarkService bookmarkService;
 
-    @GetMapping("/user/bookmarkCompany/{userId}")
-    public List<Bookmark> getAllBookmarksByUser(@PathVariable Integer userId) {
-        // userId를 사용하여 해당 유저의 북마크를 조회하는 로직을 구현하면 됩니다.
-        List<Bookmark> bookmarks = bookmarkService.getAllBookmarksByUser(userId);
-        return bookmarks;
-    }
+   @GetMapping("/user/bookmarkCompany/{userId}")
+public String getAllBookmarksByUser(@PathVariable Integer userId, HttpServletRequest request) {
+    // userId를 사용하여 해당 유저의 북마크를 조회
+    List<Bookmark> bookmarks = bookmarkService.getAllBookmarksByUser(userId);
+
+    // 조회된 북마크 목록을 HTTP 요청 객체에 추가
+    request.setAttribute("bookmarks", bookmarks);
+
+    return "bookmarks"; // 뷰 이름 반환
+}
 
 
 
