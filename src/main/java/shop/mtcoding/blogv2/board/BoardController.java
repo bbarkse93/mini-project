1af0ce59fac9860bedd52adec03a5913c972cd46
bbarkse93 <bbarkse93@gmail.com>
2023-09-04 16:00:57 +0900

    고객센터 게시글 조회

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,5 +1,9 @@
 package shop.mtcoding.blogv2.board;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +15,21 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    @GetMapping("/customerService")
-    public String customerService() {
-        return "/board/customerService";
+    @GetMapping("/csForm")
+    public String customerService(HttpServletRequest request) {
+        List<Board> boards = boardService.문의조회();
+        request.setAttribute("boards", boards);
+        return "/board/csForm";
     }
 
-    @GetMapping("/customerServiceWrite")
+    @GetMapping("/csSaveForm")
     public String customerServiceWrite() {
-        return "/board/customerServiceWrite";
+        return "/board/csSaveForm";
     }
 
     @PostMapping("/board/save")
     public String customerServiceSave(BoardRequest.SaveDTO saveDTO) {
         boardService.문의작성(saveDTO, 1);
-        return "redirect:/customerService";
+        return "redirect:/csForm";
     }
-
-    
 }
