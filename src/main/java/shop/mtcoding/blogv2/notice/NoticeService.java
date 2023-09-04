bba0312fcac879ba9c1a9424740ce21a0991417c
saki8661 <saki8661@naver.com>
2023-09-04 16:02:16 +0900

    이력서 등록하기

--- src/main/java/shop/mtcoding/blogv2/notice/NoticeService.java
+++ src/main/java/shop/mtcoding/blogv2/notice/NoticeService.java
@@ -1,14 +1,13 @@
 package shop.mtcoding.blogv2.notice;
 
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import shop.mtcoding.blogv2._core.error.ex.MyApiException;
 import shop.mtcoding.blogv2._core.error.ex.MyException;
 import shop.mtcoding.blogv2.notice.NoticeRequest.UpdateDTO;
 
