5701a0c11feff8d0d4a044fc334f53ebc4f40505
bbarkse93 <135561618+bbarkse93@users.noreply.github.com>
2023-09-04 16:07:06 +0900

    고객센터 게시글 조회 (#21)
    Co-authored-by: bbarkse93 <bbarkse93@gmail.com>

--- src/main/java/shop/mtcoding/blogv2/board/BoardService.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardService.java
@@ -1,7 +1,10 @@
 package shop.mtcoding.blogv2.board;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import shop.mtcoding.blogv2.user.User;
 
@@ -11,6 +14,7 @@ public class BoardService {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Transactional
     public void 문의작성(BoardRequest.SaveDTO saveDTO, int sessionUserId) {
         Board board = Board.builder()
                 .title(saveDTO.getTitle())
@@ -22,5 +26,10 @@ public class BoardService {
         boardRepository.save(board);
         System.out.println("test" + board);
 
+    }
+
+    public List<Board> 문의조회() {
+        List<Board> boards = boardRepository.findAll();
+        return boards;
     }
 }
