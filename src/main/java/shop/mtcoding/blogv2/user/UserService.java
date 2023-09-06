4d411b0f5f34e8e7c58ebc3f100cf7978dcaa83f
jwy4285 <jwy4285@naver.com>
2023-09-06 12:13:11 +0900

    매핑완료

--- src/main/java/shop/mtcoding/blogv2/user/UserService.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserService.java
@@ -2,6 +2,8 @@ package shop.mtcoding.blogv2.user;
 
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    HttpSession session;
 
     @Transactional
     public void 회원가입(UserRequest.JoinDTO joinDTO) {
@@ -64,13 +69,24 @@ public class UserService {
         return userRepository.findById(id).get();
     }
 
-       public ApiUtil<String> checkusername(String username) {
+    public ApiUtil<String> checkusername(String username) {
        User user = userRepository.findByUsername(username);
+      
        if (user != null) {
         throw new MyApiException("유저네임을 사용할 수 없습니다");
         
        }
        return new ApiUtil<String>(true, "유저네임을 사용할 수 있습니다");
+    }
+
+    public String 개인정보구분() {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        if (sessionUser != null && sessionUser.isDistinguish()) {
+            return "/userInformation"; // 개인회원 정보 페이지로 리다이렉트
+        } else {
+            return "/companyNoticeList"; // 기업회원 정보 페이지로 리다이렉트
+        }
     }
 
 }
