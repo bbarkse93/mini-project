435cbc45d703c08545bde87c8ea1b39593d8e475
park097 <alslalslclq24@naver.com>
2023-09-08 10:06:37 +0900

    유저 관심기업현황

---
+++ src/main/java/shop/mtcoding/blogv2/scrap/ScrapRequest.java
@@ -0,0 +1,19 @@
+package shop.mtcoding.blogv2.scrap;
+
+import lombok.Getter;
+import lombok.Setter;
+import shop.mtcoding.blogv2.notice.Notice;
+import shop.mtcoding.blogv2.user.User;
+
+public class ScrapRequest {
+
+     @Getter
+    @Setter
+    public static class UserScrapDTO {
+        private User user;
+        private Notice notice; 
+      
+    
+}  
+    
+}
